1b2574041889b335a0210fb1c4a9807ff486bfeb
Simon Angatia <simonnabuko@gmail.com>
2019-11-18 08:46:07 +0300

    appending / to api/v1/route

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ app.get('/', (req, res) => {
 
 // use routes
 
-app.use('api/v1/auth', auth);
-app.use('api/v1/articles', articles);
-app.use('api/v1/gifs', gifs);
-app.use('api/v1/profile', profile);
-app.use('api/v1/feed', feed);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/articles', articles);
+app.use('/api/v1/gifs', gifs);
+app.use('/api/v1/profile', profile);
+app.use('/api/v1/feed', feed);
 
 app.listen(port, () => {
   console.log(`The app is running on port ${port}`);
